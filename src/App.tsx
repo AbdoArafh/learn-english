1f50b48265f267d48ac1ff896b9c01177c68957f
AbdoArafh54 <abdoarafh54@gmail.com>
2021-09-09 00:09:50 +0200

    made some beginning features

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+// import logo from './logo.svg';
+import './App.scss';
+import Card from "./components/card";
+import words from "./words.json";
+import Speaker from "./components/Speaker"
 
 function App() {
+  let [wordIndex, setWordIndex] = useState(0);
+  useEffect(() => {
+    setWordIndex(+localStorage.getItem("wordIndex")! || 0);
+  }, []);
+  const speaker = new Speaker();
+  const play =  () => speaker.play(words[wordIndex].front);
+  const handleWordIndex = (isInc: boolean) => {
+    if (isInc)
+      setWordIndex((wordIndex+1) % words.length);
+    else
+      setWordIndex(wordIndex > 0 ? wordIndex - 1 : words.length - 1);
+    localStorage.setItem("wordIndex", wordIndex.toString());
+    document.getElementById("inner")!.classList.remove("flippedCard");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card front={words[wordIndex].front} back={words[wordIndex].back}/>
+      <div style={{
+        display: "flex",
+        justifyContent: "center"
+      }}>
+        <button className='new-word-btn' onClick={handleWordIndex.bind(null, false)}>
+          Previous Word
+        </button>
+        <button className="new-word-btn" onClick={handleWordIndex.bind(null, false)}>New Word</button>
+        <button onClick={play}>
+          Play
+        </button>
+      </div>
     </div>
   );
 }
